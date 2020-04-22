a789ca6766f1276946a1ad06f253678ac7988466
Tommimon <37435103+Tommimon@users.noreply.github.com>
2020-04-22 02:13:59 +0200

    aggiunti metodi statici da usare da fuori per fare controlli o modificare le mani

--- src/card.py
+++ src/card.py
@@ -31,3 +31,32 @@ class Card:
     def __init__(self, valore=NESSUN_VALORE, seme=NESSUN_SEME):
         self.valore = valore
         self.seme = seme
+
+    @staticmethod
+    def del_carta(lista, cercata):
+        for c in lista:
+            if c.seme == cercata.seme and c.valore == cercata.valore:  # se ha stessi valori
+                lista.remove(c)  # lo tolgo
+
+    @staticmethod
+    def possiede_carta(giocatore, cercata):  # è un controllo anti imbrogli di solito del server
+        for c in giocatore.player_state.mano:
+            if c.seme == cercata.seme and c.valore == cercata.valore:
+                return True
+        return False
+
+    @staticmethod
+    def possiede_seme(mano, seme):
+        for c in mano:
+            if c.seme == seme:
+                return True
+        return False
+
+    @staticmethod
+    def carta_permessa(mano, seme, carta):
+        if seme is None:  # se seme non c'è viene interpretato come se è il primo a giocare quindi sempre True
+            return True
+        if seme == carta.seme or not Card.possiede_seme(mano, seme):  # se seme giusto o se non ha quel seme
+            return True
+        return False
+
